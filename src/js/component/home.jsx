a5ed03afac2f6e67f330b82440cbf0127cba9bc6
David1997sb <salasbar97@gmail.com>
2022-01-30 19:42:35 +0000

    Adding logic and styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import TrafficLigth from "./TrafficLigth.jsx";
+import { useCallback, useEffect } from "react";
 //create your first component
 const Home = () => {
+	const [redShine, setRedShine] = useState("");
+	const [yellowShine, setYellowShine] = useState("");
+	const [greenShine, setGreenShine] = useState("");
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mainContainer">
+			<div className="container">
+				<div className="trafficTop">
+					<div className="topBar"></div>
+				</div>
+				<div className="skeleton">
+					<div className="lights">
+						<div
+							onClick={() => {
+								if (redShine === "") {
+									setRedShine("shine");
+								} else {
+									setRedShine("");
+								}
+							}}
+							className={`red ${redShine}`}></div>
+						<div
+							onClick={() => {
+								if (yellowShine === "") {
+									setYellowShine("shine");
+								} else {
+									setYellowShine("");
+								}
+							}}
+							className={`yellow ${yellowShine}`}></div>
+						<div
+							onClick={() => {
+								if (greenShine === "") {
+									setGreenShine("shine");
+								} else {
+									setGreenShine("");
+								}
+							}}
+							className={`green ${greenShine}`}></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
